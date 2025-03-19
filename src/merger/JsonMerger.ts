631d73694185c368ced304e5a0d5a49e4ac35a06
yohanim <kevin.gossent@gmail.com>
2025-03-19 13:47:18 +0100

    feat: merge and conflict rewrite with recursivity

--- src/merger/JsonMerger.ts
+++ src/merger/JsonMerger.ts
@@ -1,4 +1,4 @@
-import { castArray, differenceWith, isEqual, isNil, unionWith } from 'lodash-es'
+import { castArray, isEqual, isNil, keyBy, unionWith } from 'lodash-es' // , differenceWith
 import { KEY_FIELD_METADATA } from '../constant/metadataConstant.js'
 
 export type JsonValue =
@@ -20,80 +20,254 @@ export class JsonMerger {
    * Main entry point for merging JSON values
    */
   mergeObjects(
-    ancestor: JsonValue | undefined,
-    ours: JsonValue,
-    theirs: JsonValue
-  ): JsonValue {
-    // Handle root object (e.g., Profile)
-    if (
-      typeof ours === 'object' &&
-      ours !== null &&
-      !Array.isArray(ours) &&
-      typeof theirs === 'object' &&
-      theirs !== null &&
-      !Array.isArray(theirs)
-    ) {
-      // Get the base attribute (e.g., Profile)
-      const baseKey = Object.keys(ours)[0]
-      if (baseKey && Object.keys(theirs)[0] === baseKey) {
-        const result = { ...ours } as JsonObject
+    ancestor: JsonObject | JsonArray,
+    ours: JsonObject | JsonArray,
+    theirs: JsonObject | JsonArray,
+    parent?: JsonObject | JsonArray //,
+    // attrib?: string
+  ): JsonArray {
+    // Get all properties from three ways
+    const allProperties = new Set([
+      ...Object.keys(ancestor),
+      ...Object.keys(ours),
+      ...Object.keys(theirs),
+    ])
 
-        // Get the content of the base attribute
-        const ourContent = ours[baseKey] as JsonObject
-        const theirContent = theirs[baseKey] as JsonObject
-        const ancestorContent =
-          ancestor &&
-          typeof ancestor === 'object' &&
-          !Array.isArray(ancestor) &&
-          baseKey in ancestor
-            ? ((ancestor as JsonObject)[baseKey] as JsonObject)
-            : {}
-
-        // Get all properties from both contents
-        const allProperties = new Set([
-          ...Object.keys(ourContent),
-          ...Object.keys(theirContent),
-        ])
-
-        // Process each property
-        const mergedContent = { ...ourContent } as JsonObject
-        for (const property of allProperties) {
-          // Skip if property doesn't exist in their content
-          if (!(property in theirContent)) continue
-
-          // Use their version if property doesn't exist in our content
-          if (!(property in mergedContent)) {
-            mergedContent[property] = this.ensureArray(theirContent[property])
-            continue
+    // Process each property
+    const mergedContent = [] as JsonArray
+    for (const property of allProperties) {
+      switch (
+        this.getAttributePrimarytype(
+          ancestor[property],
+          ours[property],
+          theirs[property]
+        )
+      ) {
+        case 'object': {
+          const propObject = {}
+          propObject[property] = []
+          if (parent) {
+            propObject[property].push(
+              ...this.mergeArrays(
+                this.ensureArray(ancestor[property]),
+                this.ensureArray(ours[property]),
+                this.ensureArray(theirs[property]),
+                this.ensureArray(parent),
+                this.getKeyField(property)
+              )
+            )
+          } else {
+            propObject[property].push(
+              ...this.mergeObjects(
+                ancestor[property],
+                ours[property],
+                theirs[property],
+                propObject
+              )
+            )
           }
-
-          // Ensure both values are arrays
-          const ourArray = this.ensureArray(mergedContent[property])
-          const theirArray = this.ensureArray(theirContent[property])
-          const ancestorArray =
-            property in ancestorContent
-              ? this.ensureArray(ancestorContent[property])
-              : []
-
-          // Get the key field for this property if available
-          const keyField = this.getKeyField(property)
-
-          // Merge the arrays
-          mergedContent[property] = this.mergeArrays(
-            ancestorArray,
-            ourArray,
-            theirArray,
-            keyField
-          )
+          mergedContent.push(propObject)
+          break
         }
-
-        result[baseKey] = mergedContent
-        return result
+        default:
+          if (property.startsWith('@_') && parent) {
+            if (parent[':@']) {
+              parent[':@'][property] = ancestor[property]
+            } else {
+              parent[':@'] = {}
+              parent[':@'][property] = ancestor[property]
+            }
+          } else {
+            mergedContent.push(
+              ...this.mergeTextAttribute(
+                property,
+                ancestor[property],
+                ours[property],
+                theirs[property]
+              )
+            )
+          }
+          break
       }
     }
 
-    // Default to our version for other cases
-    return ours
+    return mergedContent
+    // Handle root object (e.g., Profile)
+    // if (
+    //   typeof ours === 'object' &&
+    //   ours !== null &&
+    //   !Array.isArray(ours) &&
+    //   typeof theirs === 'object' &&
+    //   theirs !== null &&
+    //   !Array.isArray(theirs)
+    // ) {
+    //   // Get the base attribute (e.g., Profile)
+    //   const baseKey = Object.keys(ours)[0]
+    //   if (baseKey && Object.keys(theirs)[0] === baseKey) {
+    //     const result = { ...ours } as JsonObject
+
+    //     // Get the content of the base attribute
+    //     const ourContent = ours[baseKey] as JsonObject
+    //     const theirContent = theirs[baseKey] as JsonObject
+    //     const ancestorContent =
+    //       ancestor &&
+    //       typeof ancestor === 'object' &&
+    //       !Array.isArray(ancestor) &&
+    //       baseKey in ancestor
+    //         ? ((ancestor as JsonObject)[baseKey] as JsonObject)
+    //         : {}
+
+    //     // Get all properties from both contents
+    //     const allProperties = new Set([
+    //       ...Object.keys(ourContent),
+    //       ...Object.keys(theirContent),
+    //     ])
+
+    //     // Process each property
+    //     const mergedContent = { ...ourContent } as JsonObject
+    //     for (const property of allProperties) {
+    //       // Skip if property doesn't exist in their content
+    //       if (!(property in theirContent)) continue
+
+    //       // Use their version if property doesn't exist in our content
+    //       if (!(property in mergedContent)) {
+    //         mergedContent[property] = this.ensureArray(theirContent[property])
+    //         continue
+    //       }
+
+    //       // Ensure both values are arrays
+    //       const ourArray = this.ensureArray(mergedContent[property])
+    //       const theirArray = this.ensureArray(theirContent[property])
+    //       const ancestorArray =
+    //         property in ancestorContent
+    //           ? this.ensureArray(ancestorContent[property])
+    //           : []
+
+    //       // Get the key field for this property if available
+    //       const keyField = this.getKeyField(property)
+
+    //       // Merge the arrays
+    //       mergedContent[property] = this.mergeArrays(
+    //         ancestorArray,
+    //         ourArray,
+    //         theirArray,
+    //         keyField
+    //       )
+    //     }
+
+    //     result[baseKey] = mergedContent
+    //     return result
+    //   }
+    // }
+
+    // // Default to our version for other cases
+    // return ours
+  }
+
+  private mergeTextAttribute(
+    attrib: string,
+    ancestor: JsonValue | null,
+    ours: JsonValue | null,
+    theirs: JsonValue | null
+  ): JsonArray {
+    const objAnc: JsonObject = {}
+    const objOurs: JsonObject = {}
+    const objTheirs: JsonObject = {}
+    let caseCode: number = 0
+    if (!isNil(ancestor)) {
+      objAnc[attrib] = [{ '#text': ancestor }]
+      caseCode += 100
+    }
+    if (!isNil(ours)) {
+      objOurs[attrib] = [{ '#text': ours }]
+      caseCode += 10
+    }
+    if (!isNil(theirs)) {
+      objTheirs[attrib] = [{ '#text': theirs }]
+      caseCode += 1
+    }
+    switch (caseCode) {
+      case 1:
+        return [objTheirs]
+      case 10:
+        return [objOurs]
+      case 11:
+        if (ours === theirs) {
+          return [objOurs]
+        } else {
+          const arr: JsonArray = []
+          arr.push({ '#text': '<<<<<<< LOCAL' })
+          arr.push(objOurs)
+          arr.push({ '#text': '||||||| BASE' })
+          arr.push({ '#text': '=======' })
+          arr.push(objTheirs)
+          arr.push({ '#text': '>>>>>>> REMOTE' })
+          return arr
+        }
+      case 100:
+        return []
+      case 101:
+        if (ancestor === theirs) {
+          return []
+        } else {
+          const arr: JsonArray = []
+          arr.push({ '#text': '<<<<<<< LOCAL' })
+          arr.push({ '#text': '||||||| BASE' })
+          arr.push(objAnc)
+          arr.push({ '#text': '=======' })
+          arr.push(objTheirs)
+          arr.push({ '#text': '>>>>>>> REMOTE' })
+          return arr
+        }
+      case 110:
+        if (ancestor === ours) {
+          return []
+        } else {
+          const arr: JsonArray = []
+          arr.push({ '#text': '<<<<<<< LOCAL' })
+          arr.push(objOurs)
+          arr.push({ '#text': '||||||| BASE' })
+          arr.push(objAnc)
+          arr.push({ '#text': '=======' })
+          arr.push({ '#text': '>>>>>>> REMOTE' })
+          return arr
+        }
+      case 111:
+        if (ours === theirs) {
+          return [objOurs]
+        } else if (ancestor === ours) {
+          return [objTheirs]
+        } else if (ancestor === theirs) {
+          return [objOurs]
+        } else {
+          const arr: JsonArray = []
+          arr.push({ '#text': '<<<<<<< LOCAL' })
+          arr.push(objOurs)
+          arr.push({ '#text': '||||||| BASE' })
+          arr.push(objAnc)
+          arr.push({ '#text': '=======' })
+          arr.push(objTheirs)
+          arr.push({ '#text': '>>>>>>> REMOTE' })
+          return arr
+        }
+      default:
+        return []
+    }
+  }
+
+  /**
+   * Gets the typeof of the attribute
+   */
+  private getAttributePrimarytype(
+    ancestor: JsonValue | undefined | null,
+    ours: JsonValue | undefined | null,
+    theirs: JsonValue | undefined | null
+  ): string {
+    return isNil(ancestor)
+      ? isNil(ours)
+        ? typeof theirs
+        : typeof ours
+      : typeof ancestor
   }
 
   /**
@@ -119,6 +293,7 @@ export class JsonMerger {
     ancestor: JsonArray,
     ours: JsonArray,
     theirs: JsonArray,
+    parent: JsonArray,
     keyField?: string
   ): JsonArray {
     // If no key field, use unionWith to merge arrays without duplicates
@@ -132,7 +307,7 @@ export class JsonMerger {
     }
 
     // Merge using key field
-    return this.mergeByKeyField(ancestor, ours, theirs, keyField)
+    return this.mergeByKeyField(ancestor, ours, theirs, keyField, parent)
   }
 
   /**
@@ -172,77 +347,211 @@ export class JsonMerger {
     ancestor: JsonArray,
     ours: JsonArray,
     theirs: JsonArray,
-    keyField: string
+    keyField: string,
+    parent: JsonArray
   ): JsonArray {
-    const result = [...ours]
-    const processed = new Set<string>()
-
-    // Create maps for efficient lookups
-    const ourMap = new Map<string, JsonValue>()
-    const theirMap = new Map<string, JsonValue>()
-    const ancestorMap = new Map<string, JsonValue>()
-
-    // Populate maps
-    for (const item of ours) {
-      const key = this.getItemKey(item, keyField)
-      if (key) ourMap.set(key, item)
+    const finalArray: JsonArray = []
+    let caseCode: number = 0
+    if (ancestor.length == 0) {
+      caseCode += 100
     }
-
-    for (const item of theirs) {
-      const key = this.getItemKey(item, keyField)
-      if (key) theirMap.set(key, item)
+    if (ours.length == 0) {
+      caseCode += 10
     }
-
-    for (const item of ancestor) {
-      const key = this.getItemKey(item, keyField)
-      if (key) ancestorMap.set(key, item)
+    if (theirs.length == 0) {
+      caseCode += 1
     }
+    switch (caseCode) {
+      case 1:
+        return this.mergeObjects({}, {}, theirs, parent)
+      case 10:
+        return this.mergeObjects({}, ours, {}, parent)
+      case 100:
+        return []
+    }
+    const keyedAnc = keyBy(ancestor, keyField)
+    const keyedOurs = keyBy(ours, keyField)
+    const keyedTheirs = keyBy(theirs, keyField)
+    const allKeys = new Set([
+      ...Object.keys(keyedAnc),
+      ...Object.keys(keyedOurs),
+      ...Object.keys(keyedTheirs),
+    ])
+    for (const key of allKeys) {
+      caseCode = 0
+      if (keyedAnc[key]) {
+        caseCode += 100
+      }
+      if (keyedOurs[key]) {
+        caseCode += 10
+      }
+      if (keyedTheirs[key]) {
+        caseCode += 1
+      }
+      // console.log('caseCode: ' + caseCode);
 
-    // Process items in our version
-    for (let i = 0; i < result.length; i++) {
-      const key = this.getItemKey(result[i], keyField)
-      if (!key) continue
-
-      processed.add(key)
-
-      // If item exists in both versions
-      if (theirMap.has(key)) {
-        const theirItem = theirMap.get(key)!
-        const ancestorItem = ancestorMap.get(key)
-
-        // If they changed it from ancestor but we didn't, use their version
-        if (
-          !isEqual(theirItem, ancestorItem) &&
-          isEqual(result[i], ancestorItem)
-        ) {
-          result[i] = theirItem
-        }
+      switch (caseCode) {
+        case 1:
+          finalArray.push(
+            ...this.mergeObjects({}, {}, keyedTheirs[key], parent)
+          )
+          break
+        case 10:
+          finalArray.push(...this.mergeObjects({}, {}, keyedOurs[key], parent))
+          break
+        case 100:
+          break
+        case 11:
+          if (isEqual(ours, theirs)) {
+            finalArray.push(
+              ...this.mergeObjects({}, {}, keyedOurs[key], parent)
+            )
+          } else {
+            finalArray.push({ '#text': '<<<<<<< LOCAL' })
+            finalArray.push(
+              ...this.mergeObjects({}, {}, keyedOurs[key], parent)
+            )
+            finalArray.push({ '#text': '||||||| BASE' })
+            finalArray.push({ '#text': '\n' })
+            finalArray.push({ '#text': '=======' })
+            finalArray.push(
+              ...this.mergeObjects({}, {}, keyedTheirs[key], parent)
+            )
+            finalArray.push({ '#text': '>>>>>>> REMOTE' })
+          }
+          break
+        case 101:
+          if (!isEqual(ancestor, theirs)) {
+            finalArray.push({ '#text': '<<<<<<< LOCAL' })
+            finalArray.push({ '#text': '\n' })
+            finalArray.push({ '#text': '||||||| BASE' })
+            finalArray.push(...this.mergeObjects({}, {}, keyedAnc[key], parent))
+            finalArray.push({ '#text': '=======' })
+            finalArray.push(
+              ...this.mergeObjects({}, {}, keyedTheirs[key], parent)
+            )
+            finalArray.push({ '#text': '>>>>>>> REMOTE' })
+          }
+          break
+        case 110:
+          if (!isEqual(ancestor, ours)) {
+            finalArray.push({ '#text': '<<<<<<< LOCAL' })
+            finalArray.push(
+              ...this.mergeObjects({}, {}, keyedOurs[key], parent)
+            )
+            finalArray.push({ '#text': '||||||| BASE' })
+            finalArray.push(...this.mergeObjects({}, {}, keyedAnc[key], parent))
+            finalArray.push({ '#text': '=======' })
+            finalArray.push({ '#text': '\n' })
+            finalArray.push({ '#text': '>>>>>>> REMOTE' })
+          }
+          break
+        case 111:
+          if (isEqual(ours, theirs)) {
+            finalArray.push(
+              ...this.mergeObjects({}, {}, keyedOurs[key], parent)
+            )
+          } else if (isEqual(ancestor, ours)) {
+            finalArray.push(
+              ...this.mergeObjects({}, {}, keyedTheirs[key], parent)
+            )
+          } else if (isEqual(ancestor, theirs)) {
+            finalArray.push(
+              ...this.mergeObjects({}, {}, keyedOurs[key], parent)
+            )
+          } else {
+            // finalArray.push({ '#text': '<<<<<<< LOCAL' })
+            // finalArray.push(...this.mergeObjects({}, {}, keyedOurs[key], parent))
+            // finalArray.push({ '#text': '||||||| BASE' })
+            // finalArray.push(...this.mergeObjects({}, {}, keyedAnc[key], parent))
+            // finalArray.push({ '#text': '=======' })
+            // finalArray.push(...this.mergeObjects({}, {}, keyedTheirs[key], parent))
+            // finalArray.push({ '#text': '>>>>>>> REMOTE' })
+            finalArray.push(
+              ...this.mergeObjects(
+                keyedAnc[key],
+                keyedOurs[key],
+                keyedTheirs[key],
+                parent
+              )
+            )
+          }
+          break
+        default:
       }
     }
 
-    // Add items that only exist in their version
-    const uniqueTheirItems = differenceWith(
-      Array.from(theirMap.values()),
-      result,
-      (a, b) => this.getItemKey(a, keyField) === this.getItemKey(b, keyField)
-    )
-    result.push(...uniqueTheirItems)
+    return finalArray
+    // original by scolladon
+    // const result = [...ours]
+    // const processed = new Set<string>()
 
-    return result
+    // // Create maps for efficient lookups
+    // const ourMap = new Map<string, JsonValue>()
+    // const theirMap = new Map<string, JsonValue>()
+    // const ancestorMap = new Map<string, JsonValue>()
+
+    // // Populate maps
+    // for (const item of ours) {
+    //   const key = this.getItemKey(item, keyField)
+    //   if (key) ourMap.set(key, item)
+    // }
+
+    // for (const item of theirs) {
+    //   const key = this.getItemKey(item, keyField)
+    //   if (key) theirMap.set(key, item)
+    // }
+
+    // for (const item of ancestor) {
+    //   const key = this.getItemKey(item, keyField)
+    //   if (key) ancestorMap.set(key, item)
+    // }
+
+    // // Process items in our version
+    // for (let i = 0; i < result.length; i++) {
+    //   const key = this.getItemKey(result[i], keyField)
+    //   if (!key) continue
+
+    //   processed.add(key)
+
+    //   // If item exists in both versions
+    //   if (theirMap.has(key)) {
+    //     const theirItem = theirMap.get(key)!
+    //     const ancestorItem = ancestorMap.get(key)
+
+    //     // If they changed it from ancestor but we didn't, use their version
+    //     if (
+    //       !isEqual(theirItem, ancestorItem) &&
+    //       isEqual(result[i], ancestorItem)
+    //     ) {
+    //       result[i] = theirItem
+    //     }
+    //   }
+    // }
+
+    // // Add items that only exist in their version
+    // const uniqueTheirItems = differenceWith(
+    //   Array.from(theirMap.values()),
+    //   result,
+    //   (a, b) => this.getItemKey(a, keyField) === this.getItemKey(b, keyField)
+    // )
+    // result.push(...uniqueTheirItems)
+
+    // return result
   }
 
   /**
    * Gets the key value for an item using the specified key field
    */
-  private getItemKey(item: JsonValue, keyField: string): string | undefined {
-    if (
-      typeof item === 'object' &&
-      item !== null &&
-      !Array.isArray(item) &&
-      keyField in item
-    ) {
-      return String(item[keyField])
-    }
-    return undefined
-  }
+  // private getItemKey(item: JsonValue, keyField: string): string | undefined {
+  //   if (
+  //     typeof item === 'object' &&
+  //     item !== null &&
+  //     !Array.isArray(item) &&
+  //     keyField in item
+  //   ) {
+  //     return String(item[keyField])
+  //   }
+  //   return undefined
+  // }
 }
--- src/merger/XmlMerger.ts
+++ src/merger/XmlMerger.ts
@@ -5,30 +5,38 @@ const XML_DECL = '<?xml version="1.0" encoding="UTF-8"?>\n'
 const XML_COMMENT_PROP_NAME = '#xml__comment'
 
 const parserOptions = {
-  commentPropName: XML_COMMENT_PROP_NAME,
   ignoreAttributes: false,
-  ignoreNameSpace: false,
-  numberParseOptions: { leadingZeros: false, hex: false },
-  parseAttributeValue: false,
-  parseNodeValue: false,
   parseTagValue: false,
-  processEntities: false,
-  trimValues: true,
+  parseAttributeValue: false,
+  cdataPropName: '__cdata',
+  ignoreDeclaration: true,
+  numberParseOptions: { leadingZeros: false, hex: false },
+  commentPropName: XML_COMMENT_PROP_NAME,
+  // preserveOrder: true,
 }
 
 const builderOptions = {
-  ...parserOptions,
   format: true,
   indentBy: '    ',
-  suppressBooleanAttributes: false,
-  suppressEmptyNode: false,
+  ignoreAttributes: false,
+  cdataPropName: '__cdata',
+  commentPropName: XML_COMMENT_PROP_NAME,
+  preserveOrder: true,
 }
 
 const correctComments = (xml: string): string =>
   xml.includes('<!--') ? xml.replace(/\s+<!--(.*?)-->\s+/g, '<!--$1-->') : xml
 
+const correctConflictIndent = (xml: string): string =>
+  xml
+    .replace(/[ \t]+(<<<<<<<|\|\|\|\|\|\|\||=======|>>>>>>>)/g, '$1')
+    .replace(/^[ \t]*[\n\r]+/gm, '')
+
 const handleSpecialEntities = (xml: string): string =>
-  xml.replaceAll('&amp;#160;', '&#160;')
+  xml
+    .replaceAll('&amp;#160;', '&#160;')
+    .replaceAll('&lt;', '<')
+    .replaceAll('&gt;', '>')
 
 export class XmlMerger {
   tripartXmlMerge(
@@ -41,15 +49,86 @@ export class XmlMerger {
     const ancestorObj = parser.parse(ancestorContent)
     const ourObj = parser.parse(ourContent)
     const theirObj = parser.parse(theirContent)
+    // console.log('ancestorObj')
+    // console.dir(ancestorObj, {depth:null})
 
     // Perform deep merge of XML objects
 
     const jsonMerger = new JsonMerger()
     const mergedObj = jsonMerger.mergeObjects(ancestorObj, ourObj, theirObj)
+    // console.log('mergedObj')
+    // console.dir(mergedObj, {depth:null})
 
     // Convert back to XML and format
     const builder = new XMLBuilder(builderOptions)
     const mergedXml = builder.build(mergedObj)
-    return correctComments(XML_DECL.concat(handleSpecialEntities(mergedXml)))
+    // console.log('mergedXml')
+    // console.dir(mergedXml, {depth:null})
+    return correctConflictIndent(
+      correctComments(XML_DECL.concat(handleSpecialEntities(mergedXml)))
+    )
+  }
+
+  parseThenBuild(ancestorContent: string) {
+    const parser = new XMLParser(parserOptions)
+
+    const ancestorObj = parser.parse(ancestorContent)
+    console.dir(ancestorObj, { depth: null })
+
+    // const testObj = {
+    //   CustomLabels: {
+    //     labels: {
+    //       fullName: 'tested_label',
+    //       value: '\n<<<<<<< LOCAL\nthis is ancestor label\n=======\nthis is theirs label\n>>>>>>> REMOTE\n',
+    //       language: 'fr',
+    //       protected: 'false',
+    //       shortDescription: 'this is ancestor label'
+    //     },
+    //     '@_xmlns': 'http://soap.sforce.com/2006/04/metadata'
+    //   }
+    // }
+
+    const testObj = [
+      {
+        CustomLabels: [
+          {
+            labels: [
+              { fullName: [{ '#text': 'tested_label' }] },
+              { '#text': '<<<<<<< LOCAL' },
+              { value: [{ '#text': 'this is ancestor label' }] },
+              { '#text': '||||||| base' },
+              { value: [{ '#text': 'this is ancestor label' }] },
+              { '#text': '=======' },
+              { value: [{ '#text': 'this is theirs label' }] },
+              { '#text': '>>>>>>> REMOTE' },
+              { language: [{ '#text': 'fr' }] },
+              { protected: [{ '#text': 'false' }] },
+              {
+                shortDescription: [{ '#text': 'this is ancestor label' }],
+              },
+            ],
+          },
+          {
+            labels: [
+              { fullName: [{ '#text': 'without conflict' }] },
+              { value: [{ '#text': 'all good' }] },
+              { language: [{ '#text': 'fr' }] },
+              { protected: [{ '#text': 'false' }] },
+              {
+                shortDescription: [{ '#text': 'all good' }],
+              },
+            ],
+          },
+        ],
+        ':@': { '@_xmlns': 'http://soap.sforce.com/2006/04/metadata' },
+      },
+    ]
+
+    const builder = new XMLBuilder(builderOptions)
+    const mergedXml = builder.build(testObj)
+    console.dir(mergedXml, { depth: null })
+    return correctConflictIndent(
+      correctComments(XML_DECL.concat(handleSpecialEntities(mergedXml)))
+    )
   }
 }
