0b9c2d0897359a5dd747ae1224ee715aa5c8080b
Sébastien Colladon <colladonsebastien@gmail.com>
2026-02-16 17:44:29 +0100

    chore(devops): pass OTP to scripts

--- tooling/devops-cleanup.sh
+++ tooling/devops-cleanup.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 DEV_TAG=$1
+OTP=$2
 PACKAGE="sf-git-merge-driver"
 
-npm view "${PACKAGE}" versions --json | jq -r '.[]' | grep "\\-${DEV_TAG}\\." | xargs -I {} npm deprecate "${PACKAGE}@{}" "Deprecated dev version"
-npm dist-tag rm "${PACKAGE}" "${DEV_TAG}"
+npm view "${PACKAGE}" versions --json | jq -r '.[]' | grep "\\-${DEV_TAG}\\." | xargs -I {} npm deprecate "${PACKAGE}@{}" "Deprecated dev version" --otp="${OTP}"
+npm dist-tag rm "${PACKAGE}" "${DEV_TAG}" --otp="${OTP}"
--- tooling/devops-movetag.sh
+++ tooling/devops-movetag.sh
@@ -2,6 +2,7 @@
 
 VERSION=$1
 TAG=$2
+OTP=$3
 PACKAGE="sf-git-merge-driver"
 
-npm dist-tag add "${PACKAGE}@${VERSION}" "${TAG}"
+npm dist-tag add "${PACKAGE}@${VERSION}" "${TAG}" --otp="${OTP}"
