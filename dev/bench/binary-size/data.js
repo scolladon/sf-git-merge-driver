6d2d693def84a55cdc02fc5d57b576c5c156449f
github-action-benchmark <github@users.noreply.github.com>
2026-04-21 08:53:20 +0000

    add Binary Size Benchmark (customSmallerIsBetter) benchmark result for e810eace9985c6bee4d0e5c33414741b6acae51e

---
+++ dev/bench/binary-size/data.js
@@ -0,0 +1,37 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1776761600485,
+  "repoUrl": "https://github.com/scolladon/sf-git-merge-driver",
+  "entries": {
+    "Binary Size Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "colladonsebastien@gmail.com",
+            "name": "Sebastien",
+            "username": "scolladon"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "e810eace9985c6bee4d0e5c33414741b6acae51e",
+          "message": "refactor: improve code base for performance and maintenance (#187)",
+          "timestamp": "2026-04-21T10:50:38+02:00",
+          "tree_id": "5cbd08d045607b00372607962a710cdc7704eb67",
+          "url": "https://github.com/scolladon/sf-git-merge-driver/commit/e810eace9985c6bee4d0e5c33414741b6acae51e"
+        },
+        "date": 1776761600433,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "bin/merge-driver.cjs",
+            "value": 120801,
+            "unit": "bytes"
+          }
+        ]
+      }
+    ]
+  }
+}
