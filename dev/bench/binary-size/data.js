9e005b2638318dcd9a56afaf3171c60d89fbdb9d
github-action-benchmark <github@users.noreply.github.com>
2026-04-29 13:16:01 +0000

    add Binary Size Benchmark (customSmallerIsBetter) benchmark result for 5ddb63fed21d0da03bc68dff3e31073837ce7f8d

--- dev/bench/binary-size/data.js
+++ dev/bench/binary-size/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1776763436737,
+  "lastUpdate": 1777468561096,
   "repoUrl": "https://github.com/scolladon/sf-git-merge-driver",
   "entries": {
     "Binary Size Benchmark": [
@@ -57,6 +57,35 @@ window.BENCHMARK_DATA = {
           {
             "name": "bin/merge-driver.cjs",
             "value": 120801,
+            "unit": "bytes"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "colladonsebastien@gmail.com",
+            "name": "Sebastien",
+            "username": "scolladon"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "5ddb63fed21d0da03bc68dff3e31073837ce7f8d",
+          "message": "feat(pipeline): streaming XML (parse + write) (#189)",
+          "timestamp": "2026-04-29T15:13:04+02:00",
+          "tree_id": "f38cb85753965159f8b5d43a65e8d480a3cac797",
+          "url": "https://github.com/scolladon/sf-git-merge-driver/commit/5ddb63fed21d0da03bc68dff3e31073837ce7f8d"
+        },
+        "date": 1777468561031,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "bin/merge-driver.cjs",
+            "value": 34811,
             "unit": "bytes"
           }
         ]
